154d853452b86eebe34f16feec3d0445d4679647
forhadhub <mfhe.2223.bd@gmail.com>
2025-12-16 01:13:02 +0600

    donation info change

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ import "./components/footer.js";
 import "./components/header.js";
 
 import "./pages/about_foundation.js";
-import "./pages/all_patient_stories.js";
+import "./pages/all_muntasir_billah_story.html.js";
 import "./pages/contact.js";
 import "./pages/privacy_policy.js";
 import "./pages/reg_form.js";
